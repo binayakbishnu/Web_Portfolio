7b4ddbd8124e0584b638bc57d84efe4fbd2579c4
BinayakB <bishnu.binayak12@gmail.com>
2022-02-17 00:42:33 +0530

    feat: compartmentalised contact me card

---
+++ src/ContactMe.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+import contactStyles from './ContactMe.module.css'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPhoneAlt } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faSearchLocation } from '@fortawesome/free-solid-svg-icons'
+// import { faLocationArrow } from '@fortawesome/free-solid-svg-icons'
+
+function closeContactBox() {
+    alert("Close");
+    console.log("Close");
+}
+
+function ContactMe() {
+    return (
+        <article className={`${contactStyles.contactParent}`}>
+            <section className={`${contactStyles.contactSection}`}>
+                <h2 className={`${contactStyles.contactHead}`}>My Contact Details</h2>
+
+                <div className={`${contactStyles.ContactContentMainParent}`}>
+                    <div className={`${contactStyles.ContactContentMainDiv}`}>
+                        <FontAwesomeIcon icon={faPhoneAlt} className={`${contactStyles.contactIconsMain}`} />
+                        <span className={`${contactStyles.ContactContentMain}`}>+91-9836042209</span>
+                    </div>
+                    <div className={`${contactStyles.ContactContentMainDiv}`}>
+                        <FontAwesomeIcon icon={faEnvelope} className={`${contactStyles.contactIconsMain}`} />
+                        <span className={`${contactStyles.ContactContentMain}`}>bishnu.binayak12@gmail.com</span>
+                    </div>
+                    <div className={`${contactStyles.ContactContentMainDiv}`}>
+                        <FontAwesomeIcon icon={faSearchLocation} className={`${contactStyles.contactIconsMain}`} />
+                        <span className={`${contactStyles.ContactContentMain}`}>Kolkata, West Bengal</span>
+                    </div>
+                </div>
+            </section>
+
+            <div className={`${contactStyles.contactCloseDiv}`}>
+                <input type="button" value="X" className={`${contactStyles.contactClose}`} onclick={closeContactBox} />
+            </div>
+        </article>
+    )
+}
+
+export default ContactMe
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -5,19 +5,9 @@ import React from 'react'
 
 import AboutMe from '../HomePageSections/AboutMe'
 import Welcome from '../HomePageSections/Welcome'
+import ContactMe from '../ContactMe'
 
 import homePageStyles from './HomePage.module.css'
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPhoneAlt } from '@fortawesome/free-solid-svg-icons'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
-import { faSearchLocation } from '@fortawesome/free-solid-svg-icons'
-import { faLocationArrow } from '@fortawesome/free-solid-svg-icons'
-
-function closeContactBox() {
-    alert("Close");
-    console.log("Close");
-}
 
 function HomePage() {
     return (
@@ -26,30 +16,7 @@ function HomePage() {
 
             <AboutMe />
 
-            <article className={`${homePageStyles.contactParent}`}>
-                <section className={`${homePageStyles.contactSection}`}>
-                    <h2 className={`${homePageStyles.contactHead}`}>My Contact Details</h2>
-
-                    <div className={`${homePageStyles.ContactContentMainParent}`}>
-                        <div className={`${homePageStyles.ContactContentMainDiv}`}>
-                            <FontAwesomeIcon icon={faPhoneAlt} className={`${homePageStyles.contactIconsMain}`} />
-                            <span className={`${homePageStyles.ContactContentMain}`}>+91-9836042209</span>
-                        </div>
-                        <div className={`${homePageStyles.ContactContentMainDiv}`}>
-                            <FontAwesomeIcon icon={faEnvelope} className={`${homePageStyles.contactIconsMain}`} />
-                            <span className={`${homePageStyles.ContactContentMain}`}>bishnu.binayak12@gmail.com</span>
-                        </div>
-                        <div className={`${homePageStyles.ContactContentMainDiv}`}>
-                            <FontAwesomeIcon icon={faSearchLocation} className={`${homePageStyles.contactIconsMain}`} />
-                            <span className={`${homePageStyles.ContactContentMain}`}>Kolkata, West Bengal</span>
-                        </div>
-                    </div>
-                </section>
-
-                <div className={`${homePageStyles.contactCloseDiv}`}>
-                    <input type="button" value="X" className={`${homePageStyles.contactClose}`} onclick={closeContactBox} />
-                </div>
-            </article>
+            <ContactMe />
         </div>
     )
 }
