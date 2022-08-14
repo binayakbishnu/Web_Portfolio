feef52569e4411f6a96ff7c6d6ad5e678e3c9e2b
BinayakBishnu <bishnu.binayak12@gmail.com>
2022-08-14 15:33:23 +0530

    fix: removed dummy projects

--- src/HomePageSections/Projects.jsx
+++ src/HomePageSections/Projects.jsx
@@ -106,38 +106,6 @@ function Projects() {
                         </div>
                         <p className={projectStyles.projectTechStack}>Full stack&nbsp;|&nbsp;MongoDB</p>
                     </div>
-                    <div id="project3" className={`${projectStyles.project} ${projectStyles.project3} fullStack SQL`}>
-                        <div className={`${projectStyles.container}`}>
-                            <a href="/allprojects#project1">
-                                <img src={projectPic1} alt="project1" className={`${projectStyles.projectImg} ${projectStyles.project1Img}`}
-                                    width="100%"
-                                    height="100%" />
-                                <div className={`${projectStyles.overlay}`}>
-                                    <div className={`${projectStyles.text}`}>
-                                        <h5>MediServ</h5>
-                                        <p>Online consultation/medical store</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                        <p className={projectStyles.projectTechStack}>Full stack&nbsp;|&nbsp;SQL</p>
-                    </div>
-                    <div id="project4" className={`${projectStyles.project} ${projectStyles.project4} fullStack MongoDB others`}>
-                        <div className={`${projectStyles.container}`}>
-                            <a href="/allprojects#project2">
-                                <img src={projectPic2} alt="project2" className={`${projectStyles.projectImg} ${projectStyles.project2Img}`}
-                                    width="100%"
-                                    height="100%" />
-                                <div className={`${projectStyles.overlay}`}>
-                                    <div className={`${projectStyles.text}`}>
-                                        <h5>BookRev</h5>
-                                        <p>Online book sell/buy store</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                        <p className={projectStyles.projectTechStack}>Full stack&nbsp;|&nbsp;MongoDB</p>
-                    </div>
                 </div>
             </section>
         </section >
