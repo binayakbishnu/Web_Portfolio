7cb69852a9298546aa6e6a1fe8c11f82055d65b1
BinayakBishnu <bishnu.binayak12@gmail.com>
2022-08-21 16:02:12 +0530

    feat: extended about section collapsed by default

--- src/HomePageSections/AboutMe.jsx
+++ src/HomePageSections/AboutMe.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import aboutStyles from './AboutMe.module.css'
 
 import aboutMeFig from '../assets/aboutMeFigure.svg'
 
 function AboutMe() {
+    const [toggleCollapseBtnValueEducation, setToggleCollapseBtnValueEducation] = useState("Expand");
+    const [toggleCollapseBtnValueOtherSkills, setToggleCollapseBtnValueOtherSkills] = useState("Expand");
+
+    const toggleEducationExpand = () => {
+        toggleCollapseBtnValueEducation === "Collapse" ? setToggleCollapseBtnValueEducation("Expand") : setToggleCollapseBtnValueEducation("Collapse");
+
+        document.getElementById("educationhLine").style.display =
+            document.getElementById("educationhLine").style.display === "block" ? "none" : "block";
+        document.getElementById("educationLists").style.display =
+            document.getElementById("educationLists").style.display === "block" ? "none" : "block";
+    }
+    const toggleOtherSkillsExpand = () => {
+        toggleCollapseBtnValueOtherSkills === "Collapse" ? setToggleCollapseBtnValueOtherSkills("Expand") : setToggleCollapseBtnValueOtherSkills("Collapse");
+
+        document.getElementById("otherSkillshLine").style.display =
+            document.getElementById("otherSkillshLine").style.display === "block" ? "none" : "block";
+        document.getElementById("otherSkillsLists").style.display =
+            document.getElementById("otherSkillsLists").style.display === "block" ? "none" : "block";
+    }
+
     return (
         <section className={`${aboutStyles.aboutParent}`} id="aboutme">
             <section className={`${aboutStyles.about}`} id="aboutme">
@@ -59,46 +79,56 @@ function AboutMe() {
 
                 <div className={`${aboutStyles.aboutMeExtended}`}>
                     <div className={`${aboutStyles.education}`}>
-                        <h5 className={`${aboutStyles.subHeadEducation}`}>Education</h5>
-                        <hr className={`${aboutStyles.hLine}`} />
-                        <div className={`${aboutStyles.educationList} ${aboutStyles.educationList1}`}>
-                            <p className={`${aboutStyles.educationListHead}`}>B.Tech. | Vellore Institute of Technology | 2020-24</p>
-                            <ul className={`${aboutStyles.educationListBody}`}>
-                                <li>CGPA (till 4th sem): 8.22; Latest GPA: 8.67</li>
-                                <li>Chairperson at a chapter, ISA-VIT.</li>
-                                <li>Senior core member in a club, TEC.</li>
-                                <li>Volunteered as a student coordinator of the placement cell.</li>
-                            </ul>
+                        <div className="d-flex flex-row justify-content-between align-items-center">
+                            <h5 className={`${aboutStyles.subHeadEducation}`}>Education</h5>
+                            <input type="button" value={toggleCollapseBtnValueEducation} className={`${aboutStyles.toggleCollapseBtn}`} onClick={toggleEducationExpand} />
                         </div>
-                        <div className={`${aboutStyles.educationList} ${aboutStyles.educationList2}`}>
-                            <p className={`${aboutStyles.educationListHead}`}>Class 12 | Delhi Public School, Ruby Park, Kolkata | 2018-20</p>
-                            <ul className={`${aboutStyles.educationListBody}`}>
-                                <li>AISSCE: 95.2%</li>
-                            </ul>
-                        </div>
-                        <div className={`${aboutStyles.educationList} ${aboutStyles.educationList3}`}>
-                            <p className={`${aboutStyles.educationListHead}`}>Class 10 | Delhi Public School, Ruby Park, Kolkata | 2008-18</p>
-                            <ul className={`${aboutStyles.educationListBody}`}>
-                                <li>AISSE: 94.2%</li>
-                            </ul>
+                        <hr style={{ "display": "none" }} id="educationhLine" className={`${aboutStyles.hLine}`} />
+                        <div style={{ "display": "none" }} id="educationLists">
+                            <div className={`${aboutStyles.educationList} ${aboutStyles.educationList1}`}>
+                                <p className={`${aboutStyles.educationListHead}`}>B.Tech. | Vellore Institute of Technology | 2020-24</p>
+                                <ul className={`${aboutStyles.educationListBody}`}>
+                                    <li>CGPA (till 4th sem): 8.22; Latest GPA: 8.67</li>
+                                    <li>Chairperson at a chapter, ISA-VIT.</li>
+                                    <li>Senior core member in a club, TEC.</li>
+                                    <li>Volunteered as a student coordinator of the placement cell.</li>
+                                </ul>
+                            </div>
+                            <div className={`${aboutStyles.educationList} ${aboutStyles.educationList2}`}>
+                                <p className={`${aboutStyles.educationListHead}`}>Class 12 | Delhi Public School, Ruby Park, Kolkata | 2018-20</p>
+                                <ul className={`${aboutStyles.educationListBody}`}>
+                                    <li>AISSCE: 95.2%</li>
+                                </ul>
+                            </div>
+                            <div className={`${aboutStyles.educationList} ${aboutStyles.educationList3}`}>
+                                <p className={`${aboutStyles.educationListHead}`}>Class 10 | Delhi Public School, Ruby Park, Kolkata | 2008-18</p>
+                                <ul className={`${aboutStyles.educationListBody}`}>
+                                    <li>AISSE: 94.2%</li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                     <div className={`${aboutStyles.otherSkills}`}>
-                        <h5 className={`${aboutStyles.subHeadOtherSkills}`}>Other skills</h5>
-                        <hr className={`${aboutStyles.hLine}`} />
-                        <div className={`${aboutStyles.otherSkillsList} ${aboutStyles.otherSkillsList1}`}>
-                            <p className={`${aboutStyles.otherSkillsListHead}`}>Tech</p>
-                            <ul className={`${aboutStyles.otherSkillsListBody}`}>
-                                {/* <li>Dashboarding</li> */}
-                                <li>Open Source collaboration using Git/Github</li>
-                            </ul>
+                        <div className="d-flex flex-row justify-content-between align-items-center">
+                            <h5 className={`${aboutStyles.subHeadOtherSkills}`}>Other skills</h5>
+                            <input type="button" value={toggleCollapseBtnValueOtherSkills} className={`${aboutStyles.toggleCollapseBtn}`} onClick={toggleOtherSkillsExpand} />
                         </div>
-                        <div className={`${aboutStyles.otherSkillsList} ${aboutStyles.otherSkillsList2}`}>
-                            <p className={`${aboutStyles.otherSkillsListHead}`}>Non-tech</p>
-                            <ul className={`${aboutStyles.otherSkillsListBody}`}>
-                                <li>Team Management</li>
-                                <li>Oral Communication</li>
-                            </ul>
+                        <hr style={{ "display": "none" }} id="otherSkillshLine" className={`${aboutStyles.hLine}`} />
+                        <div style={{ "display": "none" }} id="otherSkillsLists">
+                            <div className={`${aboutStyles.otherSkillsList} ${aboutStyles.otherSkillsList1}`}>
+                                <p className={`${aboutStyles.otherSkillsListHead}`}>Tech</p>
+                                <ul className={`${aboutStyles.otherSkillsListBody}`}>
+                                    {/* <li>Dashboarding</li> */}
+                                    <li>Open Source collaboration using Git/Github</li>
+                                </ul>
+                            </div>
+                            <div className={`${aboutStyles.otherSkillsList} ${aboutStyles.otherSkillsList2}`}>
+                                <p className={`${aboutStyles.otherSkillsListHead}`}>Non-tech</p>
+                                <ul className={`${aboutStyles.otherSkillsListBody}`}>
+                                    <li>Team Management</li>
+                                    <li>Oral Communication</li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
