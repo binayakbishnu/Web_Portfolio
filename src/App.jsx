37bbc05be8973ffb3ac8fecf8c21c9f01cad4a2d
BinayakBishnu <bishnu.binayak12@gmail.com>
2022-10-23 16:29:30 +0530

    fix: app.jx had redundant nav,footer element commented

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,6 @@ import {
 
 import appStyles from './App.module.css'
 
-// import Nav from './Nav';
-// import Footer from './Footer';
-
 import HomePage from './Pages/HomePage'
 import AllProjects from './Pages/AllProjects';
 import AllResearchWork from './Pages/AllResearchWork';
@@ -18,9 +15,6 @@ import AllResearchWork from './Pages/AllResearchWork';
 function App() {
   return (
     <div className={`${appStyles.app}`}>
-
-      {/* <Nav /> */}
-
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
@@ -30,9 +24,6 @@ function App() {
 
         </Routes>
       </BrowserRouter>
-
-      {/* <Footer /> */}
-
     </div>
   );
 }
