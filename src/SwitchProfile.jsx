a3a27ad527452839c79d0eb36e6df4a0d86af05f
BinayakBishnu <bishnu.binayak12@gmail.com>
2023-02-08 18:39:31 +0530

    feat: switchprofile for mobile version + css

--- src/SwitchProfile.jsx
+++ src/SwitchProfile.jsx
@@ -11,19 +11,37 @@ function SwitchProfile() {
         else navigate('/webdev');
     }
 
+    // alternative to hover for mobile view
+    const revealprofileChange = () => {
+        if (window.screen.width <= "480") {
+            console.log("clicked");
+            document.getElementById("switchProfileParentOuter").style.right = /* document.getElementById("switchProfileParentOuter").style.right === "0%" ? "-52%" : */ "0%";
+        }
+    }
+
+    const closePopUp = () => {
+        if (window.screen.width <= "414") {
+            console.log("close");
+            document.getElementById("switchProfileParentOuter").style.right = "-52%";
+        }
+    }
+
     return (
-        <div className={`${switchProfileStyles.switchProfileParentOuter}`}>
+        <div id="switchProfileParentOuter" className={`${switchProfileStyles.switchProfileParentOuter}`}>
             <section id="switchProfileParent" className={`${switchProfileStyles.switchProfileParent}`}>
-                <p>Select profile</p>
+                <p onClick={revealprofileChange}>Select profile</p>
                 <h3>Select profile</h3>
                 <div id="switchProfileContent" style={{ "display": "flex" }}>
-                    <select id="profileChange" onChange={profileChange}>
+                    <select id="profileChange" onChange={profileChange} className={`${switchProfileStyles.selectMenu}`}>
                         <option value="none" selected disabled hidden>Select an Option</option>
-                        <option>Data Analytics</option>
-                        <option>Web Developer</option>
+                        <option value="Data Analytics">Data Analytics</option>
+                        <option value="Web Developer">Web Developer</option>
                     </select>
                     {/* <input type="button" value="Go" onClick={profileChange} /> */}
                 </div>
+
+                {/* only for mobile */}
+                <button id="closeButton" onClick={closePopUp} className={`${switchProfileStyles.closeButton} text-white`}>X</button>
 
                 {/* <div className={`${switchProfileStyles.switchProfileMinDiv}`}>
                     <input type="button" value="-" className={`${switchProfileStyles.switchProfileMin}`} onClick={minSwitchProfile} target="_self" />
