14858e63573c3b25ca0f8bdd350e84652b599fa7
BinayakBishnu <bishnu.binayak12@gmail.com>
2023-02-18 20:53:46 +0530

    feat: changed tech stack section: sophisticated

--- src/HomePageSections/TechStack.jsx
+++ src/HomePageSections/TechStack.jsx
@@ -10,12 +10,14 @@ import gitLogo from '../assets/gitlogo.png'
 import mongodbLogo from '../assets/mongodblogo.png'
 import reactjsLogo from '../assets/reactjslogo.png'
 import nodejsLogo from '../assets/nodejslogo.png'
+import cppLogo from '../assets/cpplogo.png'
+import rLogo from '../assets/Rlogo.png'
 import uiuxLogo from '../assets/uiuxlogo.png'
-import figmaLogo from '../assets/figmalogo.png'
-import behanceLogo from '../assets/behancelogo.jpg'
-import dribbbleLogo from '../assets/dribbblelogo.png'
-import vscodeLogo from '../assets/vscodelogo.png'
-import jupyterLogo from '../assets/jupyterlogo.png'
+// import figmaLogo from '../assets/figmalogo.png'
+// import behanceLogo from '../assets/behancelogo.jpg'
+// import dribbbleLogo from '../assets/dribbblelogo.png'
+// import vscodeLogo from '../assets/vscodelogo.png'
+// import jupyterLogo from '../assets/jupyterlogo.png'
 
 function TechStack() {
     return (
@@ -27,16 +29,20 @@ function TechStack() {
                 </div>
 
                 <div className={`${techStackStyles.techStackMainPart}`}>
-                    <div className={`${techStackStyles.editorsSubPart}`}>
+                    {/* <div className={`${techStackStyles.editorsSubPart}`}>
                         <div className={`${techStackStyles.editorsSubPartMembers} ${techStackStyles.vscode}`}>
                             <img src={vscodeLogo} alt="VSCode" className={`${techStackStyles.techLogos} ${techStackStyles.mainTechLogos} ${techStackStyles.vscodeLogo}`} />
                         </div>
                         <div className={`${techStackStyles.editorsSubPartMembers} ${techStackStyles.jupyter}`}>
                             <img src={jupyterLogo} alt="Jupyter" className={`${techStackStyles.techLogos} ${techStackStyles.mainTechLogos} ${techStackStyles.jupyterLogo}`} />
                         </div>
-                    </div>
+                    </div> */}
 
                     <div className={`${techStackStyles.mainTech}`}>
+                        <div className={`${techStackStyles.mainTechMembers} ${techStackStyles.excel}`}>
+                            <img src={excelLogo} alt="Excel" className={`${techStackStyles.techLogos} ${techStackStyles.mainTechLogos} ${techStackStyles.excelLogo}`} />
+                            Excel
+                        </div>
                         <div className={`${techStackStyles.mainTechMembers} ${techStackStyles.sql}`}>
                             <img src={sqlLogo} alt="SQL" className={`${techStackStyles.techLogos} ${techStackStyles.mainTechLogos} ${techStackStyles.sqlLogo}`} />
                             SQL
@@ -45,10 +51,25 @@ function TechStack() {
                             <img src={tableauLogo} alt="Tableau" className={`${techStackStyles.techLogos} ${techStackStyles.mainTechLogos} ${techStackStyles.tableauLogo}`} />
                             Tableau
                         </div>
-                        <div className={`${techStackStyles.mainTechMembers} ${techStackStyles.excel}`}>
-                            <img src={excelLogo} alt="Excel" className={`${techStackStyles.techLogos} ${techStackStyles.mainTechLogos} ${techStackStyles.excelLogo}`} />
-                            Excel
+                        <div className={`${techStackStyles.mainTechMembers} ${techStackStyles.python}`}>
+                            <img src={pythonLogo} alt="Python" className={`${techStackStyles.techLogos} ${techStackStyles.mainTechLogos} ${techStackStyles.pythonLogo}`} />
+                            Python
                         </div>
+                        <div className={`${techStackStyles.mainTechMembers} ${techStackStyles.mongodb}`}>
+                            <img src={mongodbLogo} alt="MongoDB" className={`${techStackStyles.techLogos} ${techStackStyles.mainTechLogos} ${techStackStyles.mongodbLogo}`} />
+                            MongoDB
+                        </div>
+                        <div className={`${techStackStyles.mainTechMembers} ${techStackStyles.cpp}`}>
+                            <img src={cppLogo} alt="Cpp" className={`${techStackStyles.techLogos} ${techStackStyles.mainTechLogos} ${techStackStyles.cppLogo}`} />
+                            C++
+                        </div>
+                        <div className={`${techStackStyles.mainTechMembers} ${techStackStyles.r}`}>
+                            <img src={rLogo} alt="Cpp" className={`${techStackStyles.techLogos} ${techStackStyles.mainTechLogos} ${techStackStyles.rLogo}`} />
+                            R
+                        </div>
+                        {/* <div className={`${techStackStyles.mainTechMembers} ${techStackStyles.excel}`}></div> */}
+                    </div>
+                    <div className={`${techStackStyles.mainTech}`}>
                         <div className={`${techStackStyles.mainTechMembers} ${techStackStyles.reactjs}`}>
                             <img src={reactjsLogo} alt="ReactJs" className={`${techStackStyles.techLogos} ${techStackStyles.mainTechLogos} ${techStackStyles.reactjsLogo}`} />
                             ReactJs
@@ -57,28 +78,17 @@ function TechStack() {
                             <img src={nodejsLogo} alt="NodeJs" className={`${techStackStyles.techLogos} ${techStackStyles.mainTechLogos} ${techStackStyles.nodejsLogo}`} />
                             NodeJs
                         </div>
-                        <div className={`${techStackStyles.mainTechMembers} ${techStackStyles.mongodb}`}>
-                            <img src={mongodbLogo} alt="MongoDB" className={`${techStackStyles.techLogos} ${techStackStyles.mainTechLogos} ${techStackStyles.mongodbLogo}`} />
-                            MongoDB
-                        </div>
                         <div className={`${techStackStyles.mainTechMembers} ${techStackStyles.git}`}>
                             <img src={gitLogo} alt="Git" className={`${techStackStyles.techLogos} ${techStackStyles.mainTechLogos} ${techStackStyles.gitLogo}`} />
                             Git/Github
                         </div>
-                        <div className={`${techStackStyles.mainTechMembers} ${techStackStyles.python}`}>
-                            <img src={pythonLogo} alt="Python" className={`${techStackStyles.techLogos} ${techStackStyles.mainTechLogos} ${techStackStyles.pythonLogo}`} />
-                            Python
-                        </div>
-                        {/* <div className={`${techStackStyles.mainTechMembers} ${techStackStyles.hidden}`}>
-                        </div> */}
                         <div className={`${techStackStyles.mainTechMembers} ${techStackStyles.uiux}`}>
                             <img src={uiuxLogo} alt="UI/UX" className={`${techStackStyles.techLogos} ${techStackStyles.mainTechLogos} ${techStackStyles.uiuxLogo}`} />
                             UI/UX
                         </div>
-                        {/* <div className={`${techStackStyles.mainTechMembers} ${techStackStyles.excel}`}></div> */}
                     </div>
 
-                    <div className={`${techStackStyles.uiuxSubPart}`}>
+                    {/* <div className={`${techStackStyles.uiuxSubPart}`}>
                         <div className={`${techStackStyles.uiuxSubPartMembers} ${techStackStyles.figma}`}>
                             <img src={figmaLogo} alt="Figma" className={`${techStackStyles.techLogos} ${techStackStyles.figmaLogo}`} />
                         </div>
@@ -88,7 +98,7 @@ function TechStack() {
                         <div className={`${techStackStyles.uiuxSubPartMembers} ${techStackStyles.dribble}`}>
                             <img src={dribbbleLogo} alt="Dribbble" className={`${techStackStyles.techLogos} ${techStackStyles.dribbbleLogo}`} />
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </section>
         </section >
