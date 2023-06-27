141880a8602cc4805c269437215c8ec12002c01d
binayakbishnu <bishnu.binayak12@gmail.com>
2023-06-27 17:46:12 +0530

    fix: updated semester completed in education

--- src/HomePageSections/AboutMe.jsx
+++ src/HomePageSections/AboutMe.jsx
@@ -130,7 +130,7 @@ function AboutMe() {
                             <div className={`${aboutStyles.educationList} ${aboutStyles.educationList1}`}>
                                 <p className={`${aboutStyles.educationListHead}`}>B.Tech. | Vellore Institute of Technology | 2020-24</p>
                                 <ul className={`${aboutStyles.educationListBody}`}>
-                                    <li>CGPA (till 4th sem): 8.48; Latest GPA: 8.96</li>
+                                    <li>CGPA (till 6th sem): 8.48; Latest GPA: 8.96</li>
                                     <li>Chairperson at a chapter, ISA-VIT.</li>
                                     <li>Senior core member in a club, TEC.</li>
                                     <li>Volunteered as a student coordinator of the placement cell.</li>
