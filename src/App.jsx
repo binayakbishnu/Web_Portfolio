da533c4a81a37fa7bf57afc5462f776ecd7fb8e8
binayakbishnu <bishnu.binayak12@gmail.com>
2023-08-29 13:49:42 +0530

    feat: removed webdev page

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,9 @@ import appStyles from './App.module.css'
 import HomePage from './Pages/HomePage'
 import AllProjects from './Pages/AllProjects';
 import AllResearchWork from './Pages/AllResearchWork';
-import WebDev from './Pages/WebDev';
 import DataProjects from './Pages/DataProjects';
+
+// import WebDev from './Pages/WebDev';
 
 function App() {
   return (
@@ -20,7 +21,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/webdev" element={<WebDev />} />
+          {/* <Route path="/webdev" element={<WebDev />} /> */}
 
           <Route path="/allprojects" element={<AllProjects />} />
           <Route path="/allresearchWork" element={<AllResearchWork />} />
