a377f44cc8648fc1efd79a41f05835a8e8aac891
binayakbishnu <bishnu.binayak12@gmail.com>
2023-09-21 22:06:25 +0530

    feat: updated experience content

--- src/HomePageSections/Experience.jsx
+++ src/HomePageSections/Experience.jsx
@@ -15,20 +15,23 @@ function Experience() {
                     <div className={`${experienceStyles.experienceSection} ${experienceStyles.experienceSection1}`}>
                         {/* <h5>Student clubs and chapters&nbsp;&nbsp;<button className={`${experienceStyles.expandBtn}`} onClick={cCPositionToggle}>{cCPositionToggleValue}</button></h5> */}
                         Tech Lead | Core Team | eZ | February 2023-June 2023
+                        <p style={{ "fontStyle": "italic", "margin": "0" }}>Part of the pre-launch team, delegation, communication, repository admin, code ownership.</p>
                         <ul>
-                            <li>Part of the pre-launch development team.</li>
                             <li>Responsible for the front-end web development and overall management of the tech team.</li>
-                            <li>Maintained production-ready code and coordinated with the backend team.</li>
+                            <li>Understood customer requirements to enhance website experience.</li>
+                            <li>Extensively made use of Git conflict resolution and agile methodology.</li>
                         </ul>
                         Web Developer | Founder's Office | Fleverage | December 2022-February 2023
+                        <p style={{ "fontStyle": "italic", "margin": "0" }}>Member of the technical team at the incubation stage, frontend development, data-driven designing.</p>
                         <ul>
                             <li>Developed the landing page for the startup.</li>
-                            <li>Worked with the design team to decide on the brand theme and UI/UX.</li>
+                            <li>Coordinated with the design team to decide on the brand theme and UI/UX.</li>
                         </ul>
                         Web Developer, Designer | Founder's Office | Konoha Trendz | October 2021-February 2022
+                        <p style={{ "fontStyle": "italic", "margin": "0" }}>Frontend developer in the pre-launch development team.</p>
                         <ul>
-                            <li>Collaborated as a team to create the product website.</li>
-                            <li>Worked with ReactJs and NodeJs.</li>
+                            <li>Collaborated on GitHub as a team to create the product website.</li>
+                            <li>Worked with ReactJs and NodeJs on a full-stack website.</li>
                         </ul>
                     </div>
                 </div>
